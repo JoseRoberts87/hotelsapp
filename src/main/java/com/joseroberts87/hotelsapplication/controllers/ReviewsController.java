75d67dc80816c74071b39e6d33394a4763790296
joseroberts87 <webterpr@gmail.com>
2017-09-14 20:08:13 -0400

    testing reviewscontroller

--- src/main/java/com/joseroberts87/hotelsapplication/controllers/ReviewsController.java
+++ src/main/java/com/joseroberts87/hotelsapplication/controllers/ReviewsController.java
@@ -1,7 +1,28 @@
 package com.joseroberts87.hotelsapplication.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Created by rober on 9/14/2017.
  */
+
+@Controller
+@RequestMapping(value = "hotels/id/reviews")
 public class ReviewsController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    @ResponseBody
+    public String reviews(){
+        return "this is my review";
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    @ResponseBody
+    public String add(){
+        return "posted";
+    }
+
 }
